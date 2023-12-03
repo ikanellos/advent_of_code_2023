349c27bae122b7782bc9febf6ec6246ebe2f1f56
ikanellos <ilias.kanellos@gmail.com>
2023-12-03 14:04:22 +0200

    Moved star adjacent nums functionality to separate function; added explicatory comments overall

--- 3_gear_ratios/gear_ratios.py
+++ 3_gear_ratios/gear_ratios.py
@@ -60,6 +60,36 @@ def get_line_data_dict(input_line: str) -> dict:
         data_dict["num_indexes"].append(num_struct)
     # Return line data
     return data_dict
+# ---------------------------------------------------------- #
+def get_star_adjacent_nums(star: list, gear_grid: dict) -> list:
+    '''Return a list of numbers adjacent to the star coordinates, based on the gear grid'''
+
+
+    # Star coordinates
+    star_x = star[0]
+    star_y = star[1]
+    # Initial empty list
+    star_adjacent_nums = []
+
+    # Check previous line
+    if gear_grid[star_y-1]:
+        for num in gear_grid[star_y-1]["num_indexes"]:
+            if star_x >= num[1][0]-1 and star_x <= num[1][1]+1:
+                star_adjacent_nums.append(num[0])
+
+    # Check current line
+    for num in gear_grid[star_y]["num_indexes"]:
+        if star_x >= num[1][0]-1 and star_x <= num[1][1]+1:
+            star_adjacent_nums.append(num[0])        
+
+    # Check next line if exists
+    if gear_grid[star_y+1]:
+        for num in gear_grid[star_y+1]["num_indexes"]:
+            if star_x >= num[1][0]-1 and star_x <= num[1][1]+1:
+                star_adjacent_nums.append(num[0]) 
+
+    # Return list of adjacent numbers   
+    return star_adjacent_nums
     
 #############################
 # Arguments & Init
@@ -69,13 +99,23 @@ input_file = sys.argv[1]
 
 # MAIN.
 
-# We will build a dictionary of lines.
-# Keys are line indexes. Each line will
-# store index ranges of numbers and symbols,
-# along with their indexes. 
-# Each time symbols are found, we check against
-# the previous line to see if they are adjacent
-# to numbers
+# The initial idea was to build a grid where only
+# symbols of interest, i.e., numbers and non '.' symbols
+# will be stored along with their coordinates. I started
+# with a brute force approach where I would store all line
+# data, with the idea to remove it afterwards, given that
+# theoretically for task1 I could simply compare each line
+# from the file to itself and the previous one. The aim was
+# to then refine the code and not keep a complete dictionary
+# to avoid too much memory usage.
+
+# Luckily, task2 turns out is simplified a lot if we already
+# have the grid stored so that we can check it against '*' 
+# symbols. The only addition made to the code was to store
+# star symbol coordinates while doind task1 and then looping
+# over the star symbol coordinates and comparing against the
+# adjacent numbers. If adjacent numbers are 2, then we multiply
+# them and add them to the total gear sum.
 
 line_index = 0
 sum_of_valid_numbers = 0
@@ -104,7 +144,7 @@ with open(input_file) as f:
         gear_grid[line_index] = line_data_dictionary
 
         # Check numbers against line itself
-        for index in line_data_dictionary["symbol_indexes"]:
+        for index in gear_grid[line_index]["symbol_indexes"]:
             for number in line_data_dictionary["num_indexes"]:
                 index_diffs = {index-number_index for number_index in number[1]}
                 # If we have a difference of 1 or -1 we have an adjacent number
@@ -115,7 +155,7 @@ with open(input_file) as f:
         # Check numbers against previous line
         if line_index-1 in gear_grid:
             # Check symbol against previous line numbers
-            for index in line_data_dictionary["symbol_indexes"]:
+            for index in gear_grid[line_index]["symbol_indexes"]:
                 for number in gear_grid[line_index-1]["num_indexes"]:
                     # Skip over numbers already added.
                     # A number is adjacent if the symbol index falls within the number index range
@@ -124,7 +164,7 @@ with open(input_file) as f:
                         sum_of_valid_numbers += number[0]
 
             # Check number against previous line symbols
-            for number in line_data_dictionary["num_indexes"]:
+            for number in gear_grid[line_index]["num_indexes"]:
                 for index in gear_grid[line_index-1]["symbol_indexes"]:
                     # Ignore numbers already added
                     if not number[2] and index <= number[1][1]+1 and index >= number[1][0]-1:
@@ -138,28 +178,8 @@ with open(input_file) as f:
 # Check the star coordinate adjacencies
 for star in star_coords:
     # Store numbers adjacent to the star
-    star_adjacent_nums = []
-
-    star_x = star[0]
-    star_y = star[1]
-
-    # Check previous line
-    if gear_grid[star_y-1]:
-        for num in gear_grid[star_y-1]["num_indexes"]:
-            if star_x >= num[1][0]-1 and star_x <= num[1][1]+1:
-                star_adjacent_nums.append(num[0])
-
-    # Check current line
-    for num in gear_grid[star_y]["num_indexes"]:
-        if star_x >= num[1][0]-1 and star_x <= num[1][1]+1:
-            star_adjacent_nums.append(num[0])        
-
-    # Check next line if exists
-    if gear_grid[star_y+1]:
-        for num in gear_grid[star_y+1]["num_indexes"]:
-            if star_x >= num[1][0]-1 and star_x <= num[1][1]+1:
-                star_adjacent_nums.append(num[0])    
-
+    star_adjacent_nums = get_star_adjacent_nums(star, gear_grid)
+    print (f"Star adjacent nums for {star}:{star_adjacent_nums}")
     # Here the length should be 2
     if len(star_adjacent_nums) == 2:
         print (f"Star at {star} is a valid gear!")
